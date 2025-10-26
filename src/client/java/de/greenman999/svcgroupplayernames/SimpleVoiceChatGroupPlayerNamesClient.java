df2e603c2073ba5476fa3e850c5b9bf35f800e43
Greenman999 <git@greenman999.de>
2025-10-26 23:04:20 +0100

    Fabric Mod Template

---
+++ src/client/java/de/greenman999/svcgroupplayernames/SimpleVoiceChatGroupPlayerNamesClient.java
@@ -0,0 +1,10 @@
+package de.greenman999.svcgroupplayernames;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleVoiceChatGroupPlayerNamesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

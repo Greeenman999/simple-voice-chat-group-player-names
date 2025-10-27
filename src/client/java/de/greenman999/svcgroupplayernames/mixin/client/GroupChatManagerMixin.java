bf14528b292a441c590e6674508b62958c2d16f3
Greenman999 <git@greenman999.de>
2025-10-27 19:11:18 +0100

    WIP

---
+++ src/client/java/de/greenman999/svcgroupplayernames/mixin/client/GroupChatManagerMixin.java
@@ -0,0 +1,66 @@
+package de.greenman999.svcgroupplayernames.mixin.client;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import de.maxhenkel.voicechat.VoicechatClient;
+import de.maxhenkel.voicechat.voice.client.ClientVoicechat;
+import de.maxhenkel.voicechat.voice.client.GroupChatManager;
+import de.maxhenkel.voicechat.voice.client.GroupPlayerIconOrientation;
+import de.maxhenkel.voicechat.voice.common.PlayerState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = GroupChatManager.class)
+public class GroupChatManagerMixin {
+
+    @Definition(id = "drawTexture", method = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIFFIIII)V")
+    @Definition(id = "GUI_TEXTURED", field = "Lnet/minecraft/client/gl/RenderPipelines;GUI_TEXTURED:Lcom/mojang/blaze3d/pipeline/RenderPipeline;")
+    @Definition(id = "body", method = "Lnet/minecraft/entity/player/SkinTextures;body()Lnet/minecraft/util/AssetInfo$TextureAsset;")
+    @Definition(id = "texturePath", method = "Lnet/minecraft/util/AssetInfo$TextureAsset;texturePath()Lnet/minecraft/util/Identifier;")
+    @Expression("?.drawTexture(GUI_TEXTURED, ?.body().texturePath(), ?, ?, ?, ?, ?, ?, ?, ?)")
+    @WrapOperation(
+            method = "renderIcons",
+            at = @At(value = "MIXINEXTRAS:EXPRESSION", ordinal = 1)
+    )
+    private static void renderPlayerNames(DrawContext drawContext,
+                                          RenderPipeline pipeline,
+                                          Identifier sprite,
+                                          int x,
+                                          int y,
+                                          float u,
+                                          float v,
+                                          int width,
+                                          int height,
+                                          int textureWidth,
+                                          int textureHeight,
+                                          Operation<Void> original,
+                                          @SuppressWarnings("LocalMayBeArgsOnly") @Local PlayerState state,
+                                          @SuppressWarnings("LocalMayBeArgsOnly") @Local float scale,
+                                          @SuppressWarnings("LocalMayBeArgsOnly") @Local ClientVoicechat client) {
+        original.call(drawContext, pipeline, sprite, x, y, u, v, width, height, textureWidth, textureHeight);
+
+        drawContext.getMatrices().pushMatrix();
+        float invScale = 1.0f / scale;
+        drawContext.getMatrices().scale(invScale, invScale);
+
+        int nameOffsetX = x + 22;
+        int nameOffsetY = y + 5;
+
+        boolean horizontal = VoicechatClient.CLIENT_CONFIG.groupPlayerIconOrientation.get().equals(GroupPlayerIconOrientation.HORIZONTAL);
+        if (horizontal) {
+            drawContext.getMatrices().rotate((float) (Math.PI / 2));
+            nameOffsetY = y - 14;
+        }
+
+        drawContext.drawText(MinecraftClient.getInstance().textRenderer, state.getName(), nameOffsetX, nameOffsetY, client.getTalkCache().isTalking(state.getUuid()) ? 0xFFFFFFFF : 0x7FFFFFFF, false);
+
+        drawContext.getMatrices().popMatrix();
+    }
+}
